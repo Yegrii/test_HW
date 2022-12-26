8746c0713d99505f3773dd355422f0165a991b63
Yegor <viryutinyegor@gmail.com>
2022-12-26 21:16:43 +0200

    add more func, del_prod, clear_cart, etc.

--- oop_homework.py
+++ oop_homework.py
@@ -80,7 +80,7 @@ class ShoppingCart:
     def __hash__(self):
         return hash(self.products)
 
-    # функція для додавання продукту в корзину
+    # функція для додавання продукту в кошик
     def add_product(self, product, quantity=None):
         if quantity is None:
             quantity = product.unit
@@ -100,9 +100,33 @@ class ShoppingCart:
         total = 0
         for i in range(len(prod)):
             total += prod[i].price * quant[i]
+        # розумію що мабуть потрібно було зробити так
+        # return f'Total price: {round(total, 2)} UAH'
+
+        # але хотілось зробити красивіше
         return ''.join(f'Product: {prod[i].name}\n quantity: {quant[i]}\n '
             f'price: {round(prod[i].price * quant[i], 2)} UAH\n' for i in range(len(prod))) + \
-            f'Total price: {total} UAH'
+            f'Total price: {round(total, 2)} UAH\n'
+
+    # функція для видалення продукту з кошика
+    def delete_product(self, product, prod_quantity=None):
+        if prod_quantity is None:
+            del self.products[product]
+            return print(f'The product is completely removed. {product.name}--DELETED\n')
+        else:
+            self.products[product] -= prod_quantity
+            # скористався print, тому що return нічого не виводить до консолі(поки що не зрозумів чому)
+        return print(f'Deleting product:\n {product.name} in quantity {prod_quantity}'
+                     f' deleted from cart for the amount of {round(prod_quantity * product.price, 2)} UAH \n')
+    # функція для очищення кошика
+    def clear_cart(self):
+        self.products.clear()
+        return 'Cart is empty\n'
+    #  функція для видалення об'єкта ShoppingCart
+    def delete_cart(self):
+        del self.products
+        return f'Cart is deleted\n'
+
 
 # Тестування
 
@@ -121,46 +145,84 @@ if __name__ == '__main__':
     beans = Product('beans', 45.0, 1)
     sneakers_big = Product('sneakers 3 in 1', 33.45, 1)
     sneakers_small = Product('sneakers', 16.90, 1)
+    maccoffe = Product('MacCoffe', 12.0, 1)
+    potato_pink_washed = Product('potato pink washed', 33.45, 1)
+    potato_pink = Product('potato pink', 27.99, 1)
+    potato_white = Product('potato white', 21.89, 1)
 
 
     cart = ShoppingCart()
-    cart.add_product(apple, 2)
-    cart.add_product(juice, 2)
-    cart.add_product(milk, 1)
-    cart.add_product(apple, 2)
-    cart.add_product(juice, 1)
-    cart.add_product(bread, 1)
-    cart.add_product(meat, 0.7)
+    cart.add_product(apple, 4)
+    cart.add_product(juice, 5)
+    cart.add_product(milk, 3)
+    cart.add_product(meat, 1.345)
     cart.add_product(beans, 1)
-    cart.add_product(sneakers_big, 1)
-    cart.add_product(sneakers_small, 3)
+    cart.add_product(sneakers_big)
+    cart.add_product(sneakers_small)
+    cart.add_product(maccoffe, 4)
+    cart.add_product(potato_pink_washed, 1.2)
+    cart.add_product(potato_pink, 1.52)
+    cart.add_product(potato_white, 1.01)
 
+# видаляється продукт який був доданий в кошик
+    cart.delete_product(juice)
 
+# виводиться загальна сума з описом продуктів
     print(cart.get_cart_total())
 
+# видаляється продукт з вказаною кількістю
+    cart.delete_product(potato_pink, 0.52)
+
+# видалення всіх продуктів з кошика (робимо чистий кошик)
+    print(cart.clear_cart())
+
+# видаляється екземпляр класу ShoppingCart
+    print(cart.delete_cart())
+
 # Результат
+
+# видаляється продукт який був доданий в кошик
+# the product is completely removed. juice--DELETED
+
+# виводиться загальна сума з описом продуктів
 # Product: apple
-#     quantity: 4
-#     price: 92.8 UAH
-# Product: juice
-#     quantity: 3
-#     price: 148.8 UAH
+#  quantity: 4
+#  price: 92.8 UAH
 # Product: milk
-#     quantity: 1
-#     price: 53.5 UAH
-# Product: bread
-#     quantity: 1
-#     price: 25.5 UAH
+#  quantity: 3
+#  price: 160.5 UAH
 # Product: meat
-#     quantity: 0.7
-#     price: 154.7 UAH
+#  quantity: 1.345
+#  price: 297.25 UAH
 # Product: beans
-#     quantity: 1
-#     price: 45.0 UAH
+#  quantity: 1
+#  price: 45.0 UAH
 # Product: sneakers 3 in 1
-#     quantity: 1
-#     price: 33.45 UAH
+#  quantity: 1
+#  price: 33.45 UAH
 # Product: sneakers
-#     quantity: 3
-#     price: 50.7 UAH
-# Total price: 549.35 UAH
+#  quantity: 1
+#  price: 16.9 UAH
+# Product: MacCoffe
+#  quantity: 4
+#  price: 48.0 UAH
+# Product: potato pink washed
+#  quantity: 1.2
+#  price: 40.14 UAH
+# Product: potato pink
+#  quantity: 1.52
+#  price: 42.54 UAH
+# Product: potato white
+#  quantity: 1.01
+#  price: 22.11 UAH
+# Total price: 798.69 UAH
+
+# видаляється екземпляр класу ShoppingCart
+# Cart is empty
+
+
+
+
+
+
+
